d6d58eaf7fcc69f43ba7d60a6b5d28d770321e57
rohityellapu <git@stackblitz.com>
2025-02-19 12:44:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Telerik-React-data-grid-excel-export
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/rohityellapu/Telerik-React-data-grid-excel-export)
---
+++ app/main.tsx
@@ -0,0 +1,151 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {
+  ExcelExport,
+  ExcelExportColumn,
+  KendoOoxml,
+} from '@progress/kendo-react-excel-export';
+import { Loader } from '@progress/kendo-react-indicators';
+import { saveAs } from '@progress/kendo-file-saver';
+import products from './products.json';
+
+const data = products;
+let cols = [
+  'ProductID',
+  'ProductName',
+  'SupplierID',
+  'CategoryID',
+  'QuantityPerUnit',
+  'UnitPrice',
+  'UnitsInStock',
+  'UnitsOnOrder',
+  'ReorderLevel',
+  'Discontinued',
+  'ProductID',
+  'ProductName',
+  'SupplierID',
+  'CategoryID',
+  'QuantityPerUnit',
+  'UnitPrice',
+  'UnitsInStock',
+  'UnitsOnOrder',
+  'ReorderLevel',
+  'Discontinued',
+  'ProductID',
+  'ProductName',
+  'SupplierID',
+  'CategoryID',
+  'QuantityPerUnit',
+  'UnitPrice',
+  'UnitsInStock',
+  'UnitsOnOrder',
+  'ReorderLevel',
+  'Discontinued',
+  'ProductID',
+  'ProductName',
+  'SupplierID',
+  'CategoryID',
+  'QuantityPerUnit',
+  'UnitPrice',
+  'UnitsInStock',
+  'UnitsOnOrder',
+  'ReorderLevel',
+  'Discontinued',
+  'ProductID',
+  'ProductName',
+  'SupplierID',
+  'CategoryID',
+  'QuantityPerUnit',
+  'UnitPrice',
+  'UnitsInStock',
+  'UnitsOnOrder',
+  'ReorderLevel',
+  'Discontinued',
+  'ProductID',
+  'ProductName',
+  'SupplierID',
+  'CategoryID',
+  'QuantityPerUnit',
+  'UnitPrice',
+  'UnitsInStock',
+  'UnitsOnOrder',
+  'ReorderLevel',
+  'Discontinued',
+  'ProductID',
+  'ProductName',
+  'SupplierID',
+  'CategoryID',
+  'QuantityPerUnit',
+  'UnitPrice',
+  'UnitsInStock',
+  'UnitsOnOrder',
+  'ReorderLevel',
+  'Discontinued',
+  'ProductID',
+  'ProductName',
+  'SupplierID',
+  'CategoryID',
+  'QuantityPerUnit',
+  'UnitPrice',
+  'UnitsInStock',
+  'UnitsOnOrder',
+  'ReorderLevel',
+  'Discontinued',
+];
+const App = () => {
+  const [loading, setLoading] = React.useState(false);
+  const _exporter = React.createRef();
+
+  const save = async (component) => {
+    const workbook = component.current.workbookOptions();
+    const rows = workbook.sheets[0].rows;
+    let altIdx = 0;
+    rows.forEach((row) => {
+      if (row.type === 'data' && altIdx++ % 2 !== 0) {
+        row.cells.forEach((cell) => (cell.background = '#aabbcc'));
+      }
+    });
+    setLoading(true);
+    console.time('oxml');
+    const dataUrl = await new KendoOoxml.Workbook(workbook).toDataURL();
+    saveAs(dataUrl, 'Products.xlsx');
+    console.timeEnd('oxml');
+    setLoading(false);
+  };
+
+  const excelExport = () => save(_exporter);
+
+  return (
+    <div>
+      <button
+        className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base"
+        onClick={excelExport}
+      >
+        Export to Excel
+      </button>
+      {loading && (
+        <Loader
+          size="large"
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%,-50%)',
+          }}
+          type="infinite-spinner"
+        />
+      )}
+      <ExcelExport data={data} fileName="Products.xlsx" ref={_exporter}>
+        {cols.map((col, i) => (
+          <ExcelExportColumn
+            field={col}
+            title={col.toUpperCase() + i}
+            key={i}
+          />
+        ))}
+      </ExcelExport>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('my-app'));
